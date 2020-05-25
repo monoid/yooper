723cffa855d098d5ddafd8184a8a8a7521e45d34
Ellie Frost <web@stillinbeta.com>
2020-05-25 19:20:45 -0400

    Housekeeping

--- yooper/src/message.rs
+++ yooper/src/message.rs
@@ -9,26 +9,26 @@ pub struct MSearch {
     /// Maximum wait time in seconds. shall be greater than or equal to 1 and should
     /// be less than 5 inclusive.
     #[header("cache-control")]
-    max_wait: String,
+    pub max_wait: String,
     /// Field value contains Search Target.
     // TODO: enum
     #[header("st")]
-    target: String,
+    pub target: String,
     /// Field value shall begin with the following “product tokens” (defined
     /// by HTTP/1.1). The first product token identifes the operating system in the form OS name/OS version, the
     /// second token represents the UPnP version and shall be UPnP/2.0, and the third token identifes the product
     /// using the form product name/product version. For example, “USER-AGENT: unix/5.1 UPnP/2.0
     /// MyProduct/1.0”.
-    user_agent: Option<String>,
+    pub user_agent: Option<String>,
     /// control point can request that a device replies to a TCP port on the control point. When this header
     /// is present it identifies the TCP port on which the device can reply to the search.
-    tcp_port: Option<u16>,
+    pub tcp_port: Option<u16>,
     /// Specifies the friendly name of the control point. The friendly name is vendor specific.
-    friendly_name: String,
+    pub friendly_name: String,
     /// uuid of the control point. When the control point is implemented in a UPnP device it is recommended
     /// to use the UDN of the co-located UPnP device. When implemented, all specified requirements for uuid usage
     /// in devices also apply for control points.
-    uuid: Option<String>,
+    pub uuid: Option<String>,
 }
 
 #[derive(ToHeaders, FromHeaders, Debug, PartialEq)]
@@ -39,71 +39,71 @@ pub struct Available {
     /// from a root device, any of its embedded devices or any of its
     ///services, then the control point can assume that  all  are  available.
     #[header("cache-control")]
-    max_age: String,
+    pub max_age: String,
     /// Field  value  contains  a  URL  to  the  UPnP  description  of  the
     /// root  device.  Normally  the  host  portion  contains  a  literal
     /// IP  address  rather  than  a  domain  name  in  unmanaged  networks.
     /// Specified  by  UPnP  vendor. Single absolute URL (see RFC 3986)
-    location: String,
+    pub location: String,
 
-    host: String,
+    pub host: String,
 
     #[header("securelocation.upnp.org")]
-    secure_location: Option<String>,
+    pub secure_location: Option<String>,
     // TODO: Enum
     #[header("nt")]
-    notification_type: String,
+    pub notification_type: String,
     /// Field  value  shall begin  with  the  following  “product  tokens”
     /// (defined by HTTP/1.1). The first product token identifes the operating
     /// system in the form OSname/OSversion, the  second  token  represents
     /// the  UPnP version  and  shall be UPnP/2.0,  and  the  third  token
     /// identifes the product using the form productname/productversion.
-    server: String,
+    pub server: String,
 
     /// Identifies a unique instance of a device or service.
     // TODO: Enum
     #[header("usn")]
-    unique_service_name: String,
+    pub unique_service_name: String,
     /// presents the boot instance of the device expressed according to a monotonically increasing value.
     #[header("bootid.upnp.org")]
-    boot_id: Option<i32>,
+    pub boot_id: Option<i32>,
 
     #[header("configid.upnp.org")]
-    config_id: Option<i32>,
+    pub config_id: Option<i32>,
     #[header("searchport.upnp.org")]
-    search_port: Option<u16>,
+    pub search_port: Option<u16>,
 }
 
 #[derive(ToHeaders, FromHeaders, Debug, PartialEq)]
 pub struct SearchResponse {
     #[header("cache-control")]
-    max_age: String,
+    pub max_age: String,
 
-    date: Option<String>,
-    location: String,
+    pub date: Option<String>,
+    pub location: String,
 
     ext: types::Ext,
 
     #[header("securelocation.upnp.org")]
-    secure_location: Option<String>,
+    pub secure_location: Option<String>,
     server: String,
 
     // TODO: enum
     #[header("st")]
-    target: String,
+    pub target: String,
 
     // TODO: Enum
     #[header("usn")]
-    unique_service_name: String,
+    pub unique_service_name: String,
 
     /// presents the boot instance of the device expressed according to a monotonically increasing value.
     #[header("bootid.upnp.org")]
-    boot_id: Option<i32>,
+    pub boot_id: Option<i32>,
 
     #[header("configid.upnp.org")]
-    config_id: Option<i32>,
+    pub config_id: Option<i32>,
     #[header("searchport.upnp.org")]
-    search_port: Option<u16>,
+    pub search_port: Option<u16>,
 }
 
 #[derive(Debug, PartialEq, FromPacket, ToPacket)]
